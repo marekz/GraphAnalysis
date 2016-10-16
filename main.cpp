3d6b1fcd37c678e23af9494ac0c4b9a5b4cda1d7
Marek Zdybel <marek.zdybel@sbg.com.pl>
2016-10-16 19:55:54 +0200

    Inicjacja projektu

---
+++ main.cpp
@@ -0,0 +1,35 @@
+/* 
+ * File:   main.cpp
+ * Author: mzdybel
+ *
+ * Created on 16 października 2016, 17:35
+ * 
+ * Matematyka dyskretna
+ * Program zaliczeniowy
+ * Trzy cechy projektu programu:
+ * - Ma zawierać decyzję dotyczącą sposobu kodowania grafu:
+ *      Macierz incydencji
+ *      Macierz przyległości
+ *      Lista krawędzi
+ *      Tablice liniowe
+ *      Lista następników
+ * - Ma zawierać losową generację grafu i zapisanie jego kodu w programie
+ * - Analiza grafu: Co można powiedzieć ona temat grafu. Analiza ma zawierać:
+ *      a) Czy graf jest pełny, spójny, dualny, itp.
+ *      b) Czy graf jest dwudzielny lub trójdzielny
+ *      c) Czy graf zawiera drzewa, jeśli tak to program ma je wymienić
+ *      d) Końcowym efektem działania programu ma być wygenerowanie albo grafu dualnego albo grafu pełnego wyznaczonego przez twórcę programu
+ */
+
+#include <stdlib>
+
+using namespace std;
+
+/*
+ * 
+ */
+int main(int argc, char** argv) {
+
+    return 0;
+}
+
