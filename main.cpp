928229e6dd1039dd95d8038c3f7db775dd185304
Marek Zdybel <marek.zdybel@sbg.com.pl>
2016-10-16 22:45:40 +0200

    Szkic menu

--- main.cpp
+++ main.cpp
@@ -1,34 +1,83 @@
-/* 
- * File:   main.cpp
- * Author: mzdybel
- *
- * Created on 16 października 2016, 17:35
- * 
- * Matematyka dyskretna
- * Program zaliczeniowy
- * Trzy cechy projektu programu:
- * - Ma zawierać decyzję dotyczącą sposobu kodowania grafu:
- *      Macierz incydencji
- *      Macierz przyległości
- *      Lista krawędzi
- *      Tablice liniowe
- *      Lista następników
- * - Ma zawierać losową generację grafu i zapisanie jego kodu w programie
- * - Analiza grafu: Co można powiedzieć ona temat grafu. Analiza ma zawierać:
- *      a) Czy graf jest pełny, spójny, dualny, itp.
- *      b) Czy graf jest dwudzielny lub trójdzielny
- *      c) Czy graf zawiera drzewa, jeśli tak to program ma je wymienić
- *      d) Końcowym efektem działania programu ma być wygenerowanie albo grafu dualnego albo grafu pełnego wyznaczonego przez twórcę programu
- */
-
-#include <stdlib>
+#include <iostream>
+#include <cstdlib>
 
 using namespace std;
 
 /*
- * 
+ *
  */
+
 int main(int argc, char** argv) {
+
+    char NextOperation;
+    NextOperation = 'T';
+
+    while (NextOperation == 'T') {
+
+        std::system("clear");
+
+        cout << "**********************************************************************" << endl;
+        cout << "*                                                                    *" << endl;
+        cout << "*                      Matematyka dyskretna                          *" << endl;
+        cout << "*                      Program zaliczeniowy                          *" << endl;
+        cout << "*                                                                    *" << endl;
+        cout << "**********************************************************************" << endl;
+
+        cout << endl;
+        cout << endl;
+        cout << endl;
+
+        cout << "Program generuje losowe grafy o zadanej wielkości." << endl << endl;
+        cout << "Możliwe metody obliczania grafu:" << endl;
+        cout << "   A - Macierz incydencji" << endl;
+        cout << "   B - Macierz przyległości" << endl;
+        cout << "   C - Lista krawędzi" << endl;
+        cout << "   D - Tablice liniowe" << endl;
+        cout << "   E - Lista następników" << endl;
+
+        char wybor;
+
+        cout << endl;
+
+        cout << "Twoja decyzja: ";
+        cin >> wybor;
+
+        switch(wybor)
+        {
+            case 'A':
+                cout << "   A - Macierz incydencji" << endl;
+                break;
+
+            case 'B':
+                cout << "   B - Macierz przyległości" << endl;
+                break;
+
+            case 'C':
+                cout << "   C - Lista krawędzi" << endl;
+                break;
+
+            case 'D':
+                cout << "   D - Tablice liniowe" << endl;
+                break;
+
+            case 'E':
+                cout << "   E - Lista następników" << endl;
+                break;
+
+            default:
+                cout << "Błędny wybór. Możliwe opcje: A, B, C, D, E" << endl;
+
+
+        }
+
+        cout << wybor << endl;
+
+        cout << "Czy wkonać następną operację? T/N: ";
+        cin >> NextOperation;
+
+    }
+
+    cout << "Koniec programu" << endl;
 
     return 0;
 }
